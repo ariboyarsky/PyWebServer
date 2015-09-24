a3e7a41d2188b51887bb1ea9e9387bf63a07065e
Ari Boyarsky <ariboyarsky@gwu.edu>
2015-09-24 00:47:47 -0400

    add loging

--- WebServer.py
+++ WebServer.py
@@ -9,6 +9,9 @@ listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Assign a port number
 PORT = 8080
 HOST = ''
+# Create a log
+log = open('log.txt', 'a')
+log.write("Python Web Server Log \n")
 # Bind the socket to server address and server port
 listener.bind((HOST,PORT))
 # Listen to at most 1 connection at a time
@@ -16,9 +19,10 @@ listener.listen(1)
 
 # Server should be up and running and listening to the incoming connections
 print "Running on port: ", PORT
+log.write("Running on port: %s " % PORT)
 while True:
     print 'Ready to serve...'
-
+    log.write("\n Ready to serve...\n")
     # Set up a new connection from the client
     connection, addr = listener.accept()
     # If an exception occurs during the execution of try clause
@@ -30,28 +34,35 @@ while True:
         request = connection.recv(1024)
         # Extract the path of the requested object from the message
         # The path is the second part of HTTP header, identified by [1]
+        log.write("HTTP Request:\n %s \n" % request)
         req = request.split()
         path = req[1]
         # Because the extracted path of the HTTP request includes
         # a character '\', we read the path from the second character
         path = path[1:]
+        log.write("Url Path: %s" % path)
         # Store the entire content of the requested file in a temporary buffer
         f = open(path, "r")
         c = f.read();
         # Send the HTTP response header line to the connection socket
         connection.send('HTTP/1.0 200 OK\r\n')
+        log.write("\n Send HTTP 200 OK")
         connection.send("Content-type: text/html\r\n\r\n")
         # Send the content of the requested file to the connection socket
         connection.send(c)
         # Close the client connection socket
         connection.close()
+        log.write("\n Connection close \n")
 
     except IOError:
         # Send HTTP response message for file not found
         connection.send('HTTP/1.0 404 Not Found\r\n')
+        log.write("\n Send HTTP 404 Not Found")
         connection.send("Content-type: text/html\r\n\r\n")
         connection.send("<h1>404 Not Found</h1><br/>File not found: %s" % path)
         # Close the client connection socket
         connection.close()
+        log.write("\n Connection close\n")
 # Close the Server connection socket
 listener.close()
+log.write("\n Listener close")
